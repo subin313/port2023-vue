5c200f35af52a581319e400e9c0fef1d233e10a9
subin313 <82709790+subin313@users.noreply.github.com>
2023-11-01 22:23:01 +0900

    🤪 사이트 완료

---
+++ src/constants/index.js
@@ -0,0 +1,85 @@
+export const headerNav = [
+    {
+        title: "intro",
+        url: "#intro",
+    },
+    {
+        title: "skill",
+        url: "#skill",
+    },
+    {
+        title: "site",
+        url: "#site",
+    },
+    {
+        title: "portfolio",
+        url: "#port",
+    },
+    {
+        title: "contact",
+        url: "#contact",
+    },
+];
+
+export const skillText = [{
+    title:"꿈을 설계하고 디자인하다. ",
+    desc:"나는 공간을 만드는 것을 좋아한다. 어려을 때부터 나만의 공간을 만드는 것을 좋아했고 나만의 다락방을 좋아했다. 단 한 사람이라도 내가 만든 공간 속에서 영감을 받거나 마음이 움직였으면 좋겠다.나만의 공간을 마음것 만들 수 있다는 건 코딩에 엄청난 매력인거 같다. 그 한구석에 나만의 꿈을 설계하고, 개발을 하며 앞으로도 살고 싶다.",
+},
+{
+    title:"열심히 할수록 기회는 따른다.",
+    desc:"운이 좋은 사람은 없다. 단지 운을 만들 뿐이다. 운을 만들기 위해서는 내가 좋아하는 일이나 내가 하고 싶은 일에 몰두하면 된다. 몰두 하다보면 길이 보이고 방향이 보이게 된다. 운이란 고된 노동과 노력을 통해 스스로 만들어 내는 것이다.",
+},
+{
+    title:"나에게 정직하다",
+    desc:"정직은 다른 사람보다 나에게 큰 의미를 부여해야 한다. 자신이 정직하지 않으면 진정으로 원하는 일을 열정적으로 밀고 나갈 수 없다. 마음에서 우러나오는 일을 해야 정직해지며 삶을 더 즐길 줄 알게 된다.",
+}
+
+]
+
+export const introText = {
+    title:"port developer",
+    desc:["talent is","found at the end of the","effort"],
+};
+
+export const siteText = [
+    {
+        text:["make", "site compliant with", "webstandard"],
+        title:"웹 포준을 준수한 사이트 제작",
+        code:"https://github.com/subin313/port2023-vite",
+        view:"http://2023-subin-vite.netlify.app",
+        info:[
+            "site coding",
+            "production period : two days",
+            "use stack : HTML5/CSS3, CSS Variable, Vite",
+        ]
+    },
+    {
+        text:["Make","site compliant with","react.js"],
+        title:"리액트를 이용한 사이트 제작",
+        code:"https://github.com/subin313/port2023-vite",
+        view:"http://2023-subing-react.netlify.app",
+        info:[
+            "site coding",
+            "production period : two days",
+            "use stack : html5/css3. SCSS Variable, react",
+        ],
+    },
+    {
+        text:["Make","site compliant with","react.js"],
+        title:"뷰를 이용한 사이트 제작",
+        code:"https://github.com/subin313/port2023-vite",
+        view:"http://2023-subing-react.netlify.app",
+        info:[
+            "site coding",
+            "production period : two days",
+            "use stack : html5/css3. SCSS Variable, vue",
+        ],
+    },
+    {
+        text:["Make","site compliant with","react.js"],
+        title:"넥스트를 이용한 사이트 제작",
+        code:"https://github.com/subin313/port2023-vite",
+        view:"http://2023-subing-react.netlify.app",
+        info:["site coding","production period : two days","use stack : html5/css3. SCSS Variable, next"],
+    },
+]
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import {createApp}from'vue'
-import { App } from './App.vue'
+import "./assets/scss/style.scss";
+
+import {createApp}from'vue';
+import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
